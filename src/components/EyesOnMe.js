859e60214d1f2dbb174778e9d6d4b789a88ca6f2
Paul Jung <pbnjcub@gmail.com>
2022-09-04 11:40:13 -0400

    commit tree 8f0ea8c38e322b31d9c1a49f2102de63542c860e

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,36 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+// import React, { useState } from 'react'
+
+
+// In the components/EyesOnMe.js file, create a EyesOnMe React component.
+// In that component, render a button with the text 'Eyes on me'.
+// On that button, add event handlers that listen for the focus and blur events.
+// When the focus event fires, use console.log to print out the text 'Good!'.
+// When the blur event fires, use console.log to print out the text 'Hey! Eyes on me!'.
+
+function EyesOnMe(){
+    // let [eyes, setEyes] = useState(false)
+    function focus(e) {
+        e.preventDefault()
+        console.log('Good!')
+        // setEyes(false)
+    }
+
+    function blur(e) {
+        e.preventDefault()
+        console.log('Hey! Eyes on me!')
+        // setEyes(true)
+    }
+
+    return (
+        <div>
+            <form>
+                <button onBlur={blur} onFocus={focus}>Eyes on me</button>
+            {/* <button onClick={eyes ? focus : blur}>Eyes on me</button> */}
+            </form>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,24 @@
 // Code Keypad Component Here
+import React from 'react'
+
+// In the components/Keypad.js file, create a Keypad React component.
+// In that component, render an input with the right type.
+// On that input, add an event handler that listens for the change event.
+// When that event fires, use console.log to print out the text 'Entering password...'.
 
 function Keypad (){
+    function handleChange(e) {
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input
+                type='password'
+                name='keypad'
+                onChange={handleChange}
+                placeholder='Enter Password...'
+            />
+        </div>
     )
 }
 
